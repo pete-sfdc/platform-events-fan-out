7740d78039aa1831b38349afdef68be154a169fe
Pete O'Connell <peter.f.oconnell@gmail.com>
2019-11-14 12:13:37 -0600

    added debug for incoming events

--- server/config/sfdcEventConfig.js
+++ server/config/sfdcEventConfig.js
@@ -4,6 +4,7 @@ module.exports = [
     {
         eventName: "Example_Event__e",
         eventFxn: (message) => {
+            console.log('This is your message: ' + JSON.stringify(message));
             websocketService.broadcast(JSON.stringify(message));
         }
     }
